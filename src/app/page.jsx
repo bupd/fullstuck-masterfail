efa87d05ed234117b4f34de1476ce229af0ef54b
bupd <bupdprasanth@gmail.com>
2024-09-10 04:15:46 +0530

    add pages swiper cards
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import SwiperCard from "@/components/SwiperCard";
 import Image from "next/image";
 export default function Home() {
   const previousRate = "2500";
@@ -5,7 +6,7 @@ export default function Home() {
   const days = "99";
   return (
     <>
-      <div className="max-w-screen-xl mx-auto font-sans">
+      <div className="max-w-screen-xl mx-auto font-sans bg-neutral-950 text-white overflow-hidden">
         <div className="text-2xl my-8 font-bold mx-auto w-fit text-center uppercase">
           <h1>No views! no followers! no revenue</h1>
           <h1>don't worry</h1>
@@ -21,6 +22,25 @@ export default function Home() {
 
         <div className="text-lg my-4 font-thin text-slate-100 opacity-40 mx-auto w-fit text-center">
           <h1>Beginner To Pro</h1>
+        </div>
+        <div className="my-8 mx-auto lg:w-2/12 w-8/12 font-semibold flex justify-center gap-4 px-4 text-center text-white items-center">
+          <div className="bg-zinc-800 border h-[100px] flex flex-row gap-4 items-center justify-center w-full mx-auto rounded-2xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="2em"
+              height="2em"
+              viewBox="0 0 256 256"
+            >
+              <path
+                fill="#f59e0b"
+                d="m210.83 85.17l-56-56A4 4 0 0 0 152 28H56a12 12 0 0 0-12 12v72a4 4 0 0 0 8 0V40a4 4 0 0 1 4-4h92v52a4 4 0 0 0 4 4h52v124a4 4 0 0 1-4 4h-8a4 4 0 0 0 0 8h8a12 12 0 0 0 12-12V88a4 4 0 0 0-1.17-2.83M156 41.65L198.34 84H156Zm-2.06 106.85a4 4 0 0 0-4.06.11L124 164.78V160a12 12 0 0 0-12-12H48a12 12 0 0 0-12 12v48a12 12 0 0 0 12 12h64a12 12 0 0 0 12-12v-4.78l25.88 16.17a4 4 0 0 0 2.12.61a4.06 4.06 0 0 0 1.94-.5A4 4 0 0 0 156 216v-64a4 4 0 0 0-2.06-3.5M116 208a4 4 0 0 1-4 4H48a4 4 0 0 1-4-4v-48a4 4 0 0 1 4-4h64a4 4 0 0 1 4 4Zm32 .78l-24-15v-19.56l24-15Z"
+              ></path>
+            </svg>
+            <div className="flex flex-col items-baseline justify-start">
+              <h1 className="text-center font-thin text-2xl">Duration</h1>
+              <p className="text-lg font-semibold text-left">8 Hrs</p>
+            </div>
+          </div>
         </div>
 
         <div className="text-xl my-8 font-bold mx-auto w-fit text-center">
@@ -54,33 +74,32 @@ export default function Home() {
         </div>
 
         <div className="my-8 font-semibold grid grid-cols-4 gap-4 px-4 text-center text-white">
-          <div className="bg-indigo-950 rounded-lg h-[100px] flex flex-col items-center justify-center">
+          <div className="bg-indigo-950 rounded-xl h-[100px] flex flex-col items-center justify-center">
             <h1 className="text-center text-4xl">{days}</h1>
             <p className="text-sm font-thin">days</p>
           </div>
-          <div className="bg-indigo-950 rounded-lg h-[100px] flex flex-col items-center justify-center">
+          <div className="bg-indigo-950 rounded-xl h-[100px] flex flex-col items-center justify-center">
             <h1 className="text-center text-4xl">{days}</h1>
             <p className="text-sm font-thin">hours</p>
           </div>
-          <div className="bg-indigo-950 rounded-lg h-[100px] flex flex-col items-center justify-center">
+          <div className="bg-indigo-950 rounded-xl h-[100px] flex flex-col items-center justify-center">
             <h1 className="text-center text-4xl">{days}</h1>
             <p className="text-sm font-thin">minutes</p>
           </div>
-          <div className="bg-indigo-950 rounded-lg h-[100px] flex flex-col items-center justify-center">
+          <div className="bg-indigo-950 rounded-xl h-[100px] flex flex-col items-center justify-center">
             <h1 className="text-center text-4xl">{days}</h1>
             <p className="text-sm font-thin">seconds</p>
           </div>
         </div>
       </div>
-
-      <div className="max-w-screen-xl mx-auto bg-slate-900 pt-12 pb-8 font-sans">
-        <h1 className="py-8 text-center font-bold text-2xl">
+      <div className="max-w-screen-xl mx-auto text-white bg-slate-900 pt-12 pb-8 font-sans">
+        <h1 className="py-8 text-center font-bold text-3xl">
           Why this{" "}
           <span className="text-amber-500">
             Full Stack Creator Masterclass?
           </span>
         </h1>
-        <p className="text-xs px-4 pb-8 text-justify max-w-screen-lg mx-auto">
+        <p className="text-xl px-4 pb-8 text-justify max-w-screen-lg mx-auto">
           The Full Stack Creator Masterclass is designed to provide you with
           everything you need to succeed in the world of content creation, from
           beginner to pro. This comprehensive Masterclass covers all essential
@@ -89,11 +108,10 @@ export default function Home() {
           videography, and editing.
         </p>
       </div>
-
-      <div className="max-w-screen-xl overflow-hidden mx-auto bg-slate-900 font-sans">
+      <div className="max-w-screen-xl overflow-hidden text-white mx-auto bg-slate-900 font-sans">
         <div className="text-2xl py-8 pt-4 font-bold mx-auto w-fit text-center">
-          <h1 className="py-8">Start Your Journey in Content Creation</h1>
-          <ol className="text-left px-4 list-disc flex flex-col gap-2 text-xs font-normal text-slate-100 opacity-75">
+          <h1 className="py-8 px-2">Start Your Journey in Content Creation</h1>
+          <ol className="text-left pl-8 list-disc flex flex-col gap-2 text-lg font-normal text-slate-100 opacity-75">
             <li>
               When starting out, you'll handle everything for your Instagram
               page profile, posts, reels, and more.
@@ -118,12 +136,12 @@ export default function Home() {
           </ol>
         </div>
 
-        <h1 className="pt-12 pb-0 text-center font-bold text-3xl">
+        <h1 className="pt-12 pb-0 text-center font-bold text-4xl">
           Whom this <span className="text-amber-500">mastery course </span>is
           for ?
         </h1>
 
-        <div className="grid lg:grid-cols-4 grid-cols-2 gap-4 m-20">
+        <div className="grid lg:grid-cols-4 grid-cols-2 gap-4 lg:m-20 pt-4 px-4">
           <div className="bg-white rounded-2xl m-2 overflow-hidden hover:skew-y-3 hover:-skew-x-2 transition-all ease-in-out">
             <div className="hover:scale-110 transition-all flex items-center justify-center duration-200 cursor-pointer flex-col">
               <img src="/student.png" />
@@ -191,7 +209,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="max-w-screen-xl mx-auto py-8 bg-slate-950 font-sans">
+      <div className="max-w-screen-xl text-white mx-auto py-8 bg-slate-950 font-sans">
         <h1 className="py-12 text-center font-bold text-4xl">
           What will <span className="text-amber-500">you learn </span>?
         </h1>
@@ -201,7 +219,7 @@ export default function Home() {
         </p>
 
         <div className="grid gap-12 px-8 py-16">
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img src="/Mindset.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">Mindset</h1>
@@ -216,7 +234,8 @@ export default function Home() {
               </ol>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <img className="lg:hidden" src="/Business.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">Business</h1>
               <ol className="text-justify p-4 list-disc flex flex-col justify-center gap-2 text-sm font-normal text-slate-100 opacity-75">
@@ -229,9 +248,9 @@ export default function Home() {
                 <li>Understand the reasons behind wealth disparity.</li>
               </ol>
             </div>
-            <img src="/Business.png" />
+            <img className="hidden lg:block" src="/Business.png" />
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img className="h-72 mx-auto rounded-2xl" src="/Marketing.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">Marketing</h1>
@@ -245,7 +264,11 @@ export default function Home() {
               </ol>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <img
+              className="h-72 mx-auto rounded-2xl lg:hidden"
+              src="/sales.png"
+            />
             <div>
               <h1 className="text-3xl font-bold text-center">Sales</h1>
               <ol className="text-justify p-4 list-disc flex flex-col justify-center gap-2 text-sm font-normal text-slate-100 opacity-75">
@@ -256,9 +279,12 @@ export default function Home() {
                 <li>practical sales classes.</li>
               </ol>
             </div>
-            <img className="h-72 mx-auto rounded-2xl" src="/sales.png" />
+            <img
+              className="h-72 mx-auto rounded-2xl hidden lg:block"
+              src="/sales.png"
+            />
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img className="h-72 mx-auto rounded-2xl" src="/branding.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">Branding</h1>
@@ -268,7 +294,11 @@ export default function Home() {
               </ol>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <img
+              className="h-72 mx-auto rounded-2xl lg:hidden"
+              src="/gdesign.png"
+            />
             <div>
               <h1 className="text-3xl font-bold text-center">
                 Graphic Designers
@@ -288,9 +318,12 @@ export default function Home() {
                 </li>
               </ol>
             </div>
-            <img className="h-72 mx-auto rounded-2xl" src="/gdesign.png" />
+            <img
+              className="h-72 mx-auto rounded-2xl hidden lg:block"
+              src="/gdesign.png"
+            />
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img className="h-72 mx-auto rounded-2xl" src="/instaAlog.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">
@@ -303,7 +336,11 @@ export default function Home() {
               </ol>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <img
+              className="h-72 mx-auto rounded-2xl lg:hidden"
+              src="/Contentcreation.png"
+            />
             <div>
               <h1 className="text-3xl font-bold text-center">
                 Content Creation
@@ -317,11 +354,11 @@ export default function Home() {
               </ol>
             </div>
             <img
-              className="h-72 mx-auto rounded-2xl"
+              className="h-72 mx-auto rounded-2xl hidden lg:block"
               src="/Contentcreation.png"
             />
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img className="h-72 mx-auto rounded-2xl" src="/scripting.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">
@@ -334,7 +371,11 @@ export default function Home() {
               </ol>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <img
+              className="h-72 mx-auto rounded-2xl lg:hidden"
+              src="/videography.png"
+            />
             <div>
               <h1 className="text-3xl font-bold text-center">Videography</h1>
               <ol className="text-justify p-4 list-disc flex flex-col justify-center gap-2 text-sm font-normal text-slate-100 opacity-75">
@@ -345,9 +386,12 @@ export default function Home() {
                 </li>
               </ol>
             </div>
-            <img className="h-72 mx-auto rounded-2xl" src="/videography.png" />
+            <img
+              className="h-72 mx-auto rounded-2xl hidden lg:block"
+              src="/videography.png"
+            />
           </div>
-          <div className="grid grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
             <img className="h-72 mx-auto rounded-2xl" src="/editing.png" />
             <div>
               <h1 className="text-3xl font-bold text-center">Editing</h1>
@@ -371,12 +415,46 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="max-w-screen-xl mx-auto text-white bg-slate-950">
         <h1 className="py-12 text-center font-bold text-4xl">
-           <span className="text-amber-500">Testimonials </span>
+          <span className="text-amber-500">Testimonials </span>
         </h1>
-        <h1 className="pb-12 text-center font-bold text-4xl">
-        What They Say?
+        <h1 className="pb-12 text-center font-bold text-4xl">What They Say?</h1>
+      </div>
+      <div className="bg-slate-950">
+        <SwiperCard />
+      </div>
+      <div className="max-w-screen-xl text-white flex flex-col items-center mx-auto bg-slate-950 font-sans py-12">
+        <h1 className="py-12 text-center font-bold text-4xl">
+          What I get from this{" "}
+          <span className="text-amber-500">masterclass </span>?
         </h1>
+        <ol className="pl-12 text-wrap list-decimal flex flex-col justify-center gap-2  font-normal text-slate-100 opacity-85 text-2xl">
+          <li>Trending assets pack worth 3,999.</li>
+          <li>Content creation mastery ebook worth 2,999.</li>
+          <li>Ai tools pdf worth 1,999.</li>
+          <li>Placement- partime.</li>
+          <li>Community Access.</li>
+          <li>1-1 page review.</li>
+          <li>Certificate.</li>
+        </ol>
+      </div>
+      <div className="max-w-screen-xl text-white overflow-hidden mx-auto py-12 bg-slate-950 px-16">
+        <h1 className="py-12 text-center font-bold text-4xl">
+          Join the{" "}
+          <span className="text-amber-500">
+            Full Stack Creator Masterclass{" "}
+          </span>
+          ?
+        </h1>
+        <p className="text-justify text-xl pt-4 max-w-4xl mx-auto pb-8">
+          Whether you're a beginner or looking to enhance your skills, this
+          masterclass provides a complete roadmap to becoming a successful
+          content creator. Grow your audience and turn your passion into a
+          thriving business. Join the Full Stack Creator Masterclass and
+          transform your creative journey today.
+        </p>
+      </div>
     </>
   );
 }
