d4dcd535323d5919ba830ccb04a2338bb6eb7e79
bupd <bupdprasanth@gmail.com>
2024-09-18 23:33:53 +0530

    add video route
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ src/app/video/page.jsx
@@ -0,0 +1,18 @@
+export default function Video() {
+  return (
+    <>
+      <div className="bg-neutral-950">
+        <div className="max-w-screen-xl mx-auto font-sans bg-neutral-950 text-white overflow-hidden">
+          <div className="text-2xl my-8 font-bold mx-auto w-fit text-center uppercase">
+            <h1>No views! no followers! no revenue</h1>
+            <h1>don't worry</h1>
+          </div>
+          <div className="text-2xl my-8 font-bold mx-auto w-fit text-center">
+            <h1>Full Stack Creator</h1>
+            <h1>Masterclass</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
