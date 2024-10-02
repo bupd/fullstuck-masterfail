7ef07fae53efd163cc10c8905921f0ad600bcda6
bupd <bupdprasanth@gmail.com>
2024-10-02 18:12:02 +0530

    update redirect with loading
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/app/payment-success/RedirectandOpenTab.jsx
+++ src/app/payment-success/RedirectandOpenTab.jsx
@@ -1,22 +1,45 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 const RedirectAndOpenTab = ({ isPaidUser }) => {
-  const router = useRouter(); // useRouter from next/navigation
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (isPaidUser) {
-      // Open a new tab with the WhatsApp link
-      window.open("https://chat.whatsapp.com/CO4H6BtMAQ3ApTdxRCX87s", "_blank");
+      // Set loading state to true and show modal for 8 seconds
+      setLoading(true);
 
-      // Redirect the current tab to the '/videos' page
-      router.push("/videos");
+      const timer = setTimeout(() => {
+        // Open the new tab with the WhatsApp link
+        window.open("https://chat.whatsapp.com/CO4H6BtMAQ3ApTdxRCX87s", "_blank");
+
+        // Redirect to the '/videos' page after 8 seconds
+        router.push("/videos");
+
+        setLoading(false)
+      }, 8000); // 8-second delay
+
+      return () => clearTimeout(timer); // Cleanup timer when component unmounts
     }
   }, [isPaidUser, router]);
 
-  return <div>Redirecting...</div>;
+  return (
+    <div>
+      {/* Modal to show loading */}
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-3xl shadow-lg animate-bounce">
+            <h2 className="text-lg font-semibold mb-2">Loading...</h2>
+            <p>Please wait while we redirect you.</p>
+          </div>
+        </div>
+      )}
+
+    </div>
+  );
 };
 
 export default RedirectAndOpenTab;
